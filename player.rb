376013f24fb77e8dc5c2a92dbd2fe399be35eef3
gonjavi <javiercaliescali@gmail.com>
2019-08-06 17:56:31 -0500

    class player tested

--- player.rb
+++ player.rb
@@ -6,14 +6,6 @@ class Player
          @player== "x" ? @player="o" : @player="x"
          
       end
-      def print
-        puts "#{@player}"
-      end
+      
 end
 
-player=Player.new
-player.print
-player.change_player
-player.print
-player.change_player
-player.print
