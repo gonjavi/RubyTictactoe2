fcc6d8f7a0bd3738698f96cbe9a20e1b11d1fc9b
gonjavi <javiercaliescali@gmail.com>
2019-08-06 09:22:32 -0500

    player class working

--- player.rb
+++ player.rb
@@ -1,5 +1,19 @@
 class Player
+    def initialize
+        @player="x"
+    end
      def change_player
          @player== "x" ? @player="o" : @player="x"
+         
+      end
+      def print
+        puts "#{@player}"
       end
 end
+
+player=Player.new
+player.print
+player.change_player
+player.print
+player.change_player
+player.print
