874096c6a08c5c3c241090bf587ce7ff576a9f55
gonjavi <javiercaliescali@gmail.com>
2019-08-07 10:14:23 -0500

    changes in class turn

--- turn.rb
+++ turn.rb
@@ -1,40 +1,60 @@
 class Turn
     attr_accessor :b, :board, :player
     def initialize(board=["-", "-", "-", "-", "-", "-", "-", "-", "-"] ,player)
-        @b=0
+        b=0
         @board=board
-        @player=player
+        player=player
     end
-    def turn
+    def turn(board,player)
+        state=true
+        while state
         # @b recibe x o o y check board mira si no ha sido ocupada
             puts "choose the number for the position"
-            @b=gets.chomp
-            if  @b =~ /\d/
-                check_board
-                 
+            b=gets.chomp
+            if  b =~ /\d/
+                    #continue to check board
+                    b=b.to_i
+                    b-=1
+                    state=false
+                    if @board[b] != "-"          
+                        puts "the number has been selected choose another number"
+                        state=true
+                    end
              else
                 puts "Please input a numerical position (1-9)"
-                turn
+              
              end
+             
             
-            put_on_board
+        end  
+         #put on board
+         if player=="x"
+            @board[b]="x"
+
+         elsif player=="o"
+           @board[b]="o"
+         end
+           return @board
     end
+
+
+
       #board parameter is an array with the positions to be checked
-    def check_board
-            @b=@b.to_i
-         @b-=1
-         if @board[@b] != "-"          
+    def check_board(board,b)
+            b=b.to_i
+         b-=1
+         if board[b] != "-"          
             puts "the number has been selected choose another number"
             turn
          end
     end
 
     def put_on_board
-        if @player=="x"
-            @board[@b]="x"
+        if player=="x"
+            board[b]="x"
 
-        elsif @player=="o"
-            @board[@b]="o"
+        elsif player=="o"
+            board[b]="o"
         end
     end
 
