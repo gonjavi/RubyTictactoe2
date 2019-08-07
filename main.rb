567548d8d6a15f16786a603386b2c3db8d940299
gonjavi <javiercaliescali@gmail.com>
2019-08-07 07:26:13 -0500

    testing

--- main.rb
+++ main.rb
@@ -1,10 +1,27 @@
 require_relative 'lib/classes.rb'
+board=Board.new
+check_win=Check_win.new
+check_draw=Check_draw.new
+
+player=Player.new
+turn=Turn.new(board.board,player)
 
 puts "Tic Tac Toe"
 
 
-board=Board.new
+
 board.show_board
 
-check_win=Check_win.new
-check_win.check_win
+
+check_win.check_win(board.board)
+
+
+check_draw.check_draw(board.board)
+
+
+
+
+turn.turn
+
+
+player.change_player
