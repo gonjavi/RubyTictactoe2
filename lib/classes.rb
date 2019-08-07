0ac9c6455e01def547f14eec35ad35fb66d87a81
gonjavi <javiercaliescali@gmail.com>
2019-08-06 23:04:29 -0500

    adding requires to classes file

---
+++ lib/classes.rb
@@ -0,0 +1,5 @@
+require_relative './classes/player.rb'
+require_relative './classes/turn.rb'
+require_relative './classes/board.rb'
+require_relative './classes/check_win.rb'
+require_relative './classes/check_draw.rb'
---
+++ main.rb
@@ -0,0 +1,1 @@
+require_relative 'lib/classes.rb'
