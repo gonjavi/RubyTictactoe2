8ef44cfb369bb4e52b0de1c9e89cbf28c58e1ceb
gonjavi <javiercaliescali@gmail.com>
2019-08-08 08:14:33 -0500

    testing

--- main.rb
+++ main.rb
@@ -6,18 +6,19 @@ check_draw=Check_draw.new
 player=Player.new
 turn=Turn.new(board.board,player)
 
+
 @play_game = true
-while @play_game
+#while @play_game
         puts "Tic Tac Toe"
 
         board.show_board
+        board=board.board
+        check_win.check_win(board)
 
-        check_win.check_win(board.board)
+        check_draw.check_draw(board)
 
-        check_draw.check_draw(board.board)
-
-        turn.turn
-        turn.check_board
-        turn.put_on_board
+        board=turn.turn(board,player)
+        #urn.check_board
+        #turn.put_on_board
         player.change_player
-end
+#end
